7cae3338f210f2d08e863d8736744a207f7ac4b2
Petrisor29 <petrisor.gnr@gmail.com>
2026-01-07 22:30:03 +0200

    Create SmallerNumbersThanCurrent.java

---
+++ src/main/java/com/leetcode/problems/SmallerNumbersThanCurrent.java
@@ -0,0 +1,25 @@
+package com.leetcode.problems;
+
+/**
+ * Problem: 1365. How Many Numbers Are Smaller Than the Current Number
+ * Link: https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+ * Difficulty: Easy
+ *
+ * Time Complexity: O(n^2) - Brute Force (doua bucle for imbricate).
+ * Space Complexity: O(n) - Pentru array-ul de rezultat.
+ */
+public class SmallerNumbersThanCurrent {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] result = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for(int j = 0; j < nums.length; j++) {
+                if(nums[j] < nums[i]){
+                    result[i] = ++count;
+                }
+            }
+        }
+        return result;
+    }
+}
